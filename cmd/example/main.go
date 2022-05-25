9207a63d4e4acecc0ede9d59f4555da577d0b9f6
Ilya Senchukov <senchukov02@gmail.com>
2022-05-25 10:07:33 +0300

    feat: restructure code

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,58 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
-	"os"
-	"strings"
 
-	lab2 "github.com/Beaxhem/architecture-lab-2"
+	handler "github.com/Beaxhem/architecture-lab-2/handler"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
 	inputPath       = flag.String("f", "", "Path to file with expression to compute")
-	outputPath      = flag.String("o", "", "Path to file to write the result")
+	outputPath      = flag.String("o", "", "Path to file to write the result to")
 )
 
 func main() {
 	flag.Parse()
-	handler := lab2.ComputeHandler{
-		Reader: reader(inputExpression, inputPath),
-		Writer: writer(outputPath),
+	handler, err := handler.NewComputeHandler(inputExpression, inputPath, outputPath)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	err := handler.Compute()
+	err = handler.Compute()
 	if err != nil {
 		fmt.Println(err)
 	}
-}
-
-// Returns io.Reader depending on inputExpression and inputPath.
-// If inputExpression is not empty, returns strings.NewReader(inputExpression).
-// If inputPath is not empty, returns os.Open(inputPath).
-// If both inputExpression and inputPath are empty, returns os.Stdin.
-func reader(inputExpression *string, inputPath *string) io.Reader {
-	if inputExpression != nil && *inputExpression != "" {
-		return strings.NewReader(*inputExpression)
-	}
-	if inputPath != nil && *inputPath != "" {
-		reader, err := os.Open(*inputPath)
-		if err != nil {
-			return nil
-		}
-		return reader
-	}
-	return os.Stdin
-}
-
-// Returns io.Writer depending on outputPath. If outputPath is empty, returns os.Stdout.
-func writer(outputPath *string) io.Writer {
-	if outputPath != nil && *outputPath != "" {
-		writer, err := os.Create(*outputPath)
-		if err != nil {
-			return nil
-		}
-		return writer
-	}
-	return os.Stdout
 }
---
+++ tests/handler_test.go
@@ -0,0 +1,14 @@
+package tests
+
+import (
+	"testing"
+
+	. "github.com/Beaxhem/architecture-lab-2/handler"
+	. "gopkg.in/check.v1"
+)
+
+func HandlerTest(t *testing.T) { TestingT(t) }
+
+type HandlerSuite struct{}
+
+var _ = Suite(&HandlerSuite{})
